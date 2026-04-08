d6efea34c30302ab10eee4c76f1c186ae8c8b44f
Pedro Camargo Brito <pedro.ca.brito@hotmail.com>
2026-04-08 18:28:54 -0300

    Make the 2.6 to 2.17 exercicies

--- part2/phonebook_2.6-2.17/src/components/PhoneBook.js
+++ part2/phonebook_2.6-2.17/src/components/PhoneBook.js
@@ -4,12 +4,14 @@ import InputFieldWithName from './InputFieldWithName'
 import PersonForm from './PersonForm'
 import PersonList from './PersonList'
 import PhoneBookService from '../services/PhoneBookService'
+import Notification from './Notification'
 
 const PhoneBook = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchTerm, setSearchTerm] = useState('')
+    const [notification, setNotification] = useState({ type: null, message: null })
 
     const addNewPerson = (event) => {
         event.preventDefault()
@@ -21,10 +23,16 @@ const PhoneBook = () => {
         }
 
         const newPerson = { name: newName, number: newNumber }
-        PhoneBookService.create(newPerson).then((returnedPerson) => {
+        PhoneBookService.create(newPerson)
+        .then((returnedPerson) => {
             setPersons(persons.concat(returnedPerson))
             setNewName('')
             setNewNumber('')
+        }).then(() => {
+            setNotification({ type: 'success', message: `Added ${newName}` })
+            setTimeout(() => {
+              setNotification({ type: null, message: null })
+            }, 5000)
         })
     }
 
@@ -36,7 +44,11 @@ const PhoneBook = () => {
                     setPersons(persons.map((person) => person.id !== returnedPerson.id ? person : returnedPerson))
                 })
                 .catch((error) => {
-                    alert('Failed to update the person. Please try again.', error)
+                    setPersons(persons.filter((person) => person.id !== personFound.id))
+                    setNotification({ type: 'error', message: `Information of ${personFound.name} has already been removed from server` })
+                    setTimeout(() => {
+                        setNotification({ type: null, message: null })
+                    }, 5000)
                 })
             setNewName('')
             setNewNumber('')
@@ -59,7 +71,10 @@ const PhoneBook = () => {
                     setPersons(persons.filter((person) => person.id !== id))
                 })
                 .catch((error) => {
-                    alert('Failed to delete the person. Please try again.', error)
+                    setNotification({ type: 'error', message: `Failed to delete` })
+                    setTimeout(() => {
+                        setNotification({ type: null, message: null })
+                    }, 5000)
                 })
         }
     }
@@ -75,6 +90,7 @@ const PhoneBook = () => {
     return (
         <div>
             <Title text="Phonebook" />
+            <Notification  type={notification.type} message={notification.message} />
             <InputFieldWithName text="filter shown with" value={searchTerm} onChange={searchByPersonName} />
 
             <Title text="add a new" />
@@ -84,6 +100,7 @@ const PhoneBook = () => {
 
             <Title text="Numbers" />
             <PersonList persons={filteredPersons} onDeletePerson={onDeletePerson} />
+            
         </div>
     )
 }
